3d140e17f6a8180a396e8e734711a678896d8ba3
larsmhar <lars.martin.hardeland@gmail.com>
2019-10-15 20:13:24 +0200

    Fixed doggie command

--- commands/doggie.js
+++ commands/doggie.js
@@ -2,30 +2,32 @@ const fetch = require('node-fetch');
 
 exports.run = async (client, message, args) => {
   console.log("dog")
+  // TODO: remove .replace and clean up code
   // From https://api.woofbot.io/v1/breeds
   let breeds = [
-    "Corgi",
-    "Shiba",
-    "Golden Retriever",
-    "Pitbull",
-    "Husky",
-    "Samoyed",
-    "Beagle",
-    "Cocker Spaniel",
-    "German Shepherd",
-    "Greyhound",
-    "Pomeranian",
-    "Dachshund",
-    "Boxer"
+    "corgi",
+    "shiba",
+    "golden_retriever",
+    "pitbull",
+    "husky",
+    "samoyed",
+    "beagle",
+    "cocker_spaniel",
+    "german_shepherd",
+    "greyhound",
+    "pomeranian",
+    "dachshund",
+    "boxer"
   ];
-  console.log(args)
-  let searchBreed = breeds[breeds.findIndex[args[1]]] || breeds[Math.floor(Math.random() * breeds.length)] 
-  console.log("link", link)
-  let val = await fetch(`https://api.woofbot.io/v1/breeds/${breed}/image`)
+  if (args[0] === "breeds") {
+    message.channel.send(breeds); return
+  }
+  let searchBreed = breeds[breeds.indexOf(args[0])] || breeds[Math.floor(Math.random() * breeds.length)] 
+  let link = await fetch(`https://api.woofbot.io/v1/breeds/${searchBreed.replace("_", " ")}/image`)
     .then(x => x.json())
     .then(x => x['response']['url'])
     .catch(error => {console.log(error)})
-  message.channel.send(`Here's a nice lil' ${searchBreed}`, {
+  message.channel.send(`Here's a nice lil' ${searchBreed.replace("_", " ")}`, {
       file: link
   });
 }
